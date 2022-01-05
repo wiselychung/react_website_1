27fbaecd9b4d8455b3a1ef23bca138105ca81aee
Wisely <wiselychung96@gmail.com>
2022-01-05 13:24:26 +0800

    created about and home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
 import PageWrapper from './components/PageWrapper';
-import Home from './components/Pages/Home';
 import {BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams} from 'react-router-dom';
+
+// Pages
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
 
 class App extends Component {
   render(){
     return (
       <Router>
         <PageWrapper>
-          <Route path = "/" component = {Home} />
+          <Route
+            exact = {true}
+            path = "/" 
+            component = {Home} 
+          />
+          
+          <Route
+            path="/about"
+            component={About}
+          />
+          
         </PageWrapper>
       </Router>
     );
